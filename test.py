e31d06816f7abde02b80ad55448cdae4f4336f32
Kirill <kirbo06@mail.ru>
2024-11-29 19:22:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 file_path = "content/index.md"
 
-print("test")
+print("Work dir: ", os.getcwd())
+print("file exist: ", os.path.exists(file_path))
 
 with open(file_path, "r") as file:
     content = file.read()
 
 new_content = content.replace("%%replace%%", "THIS IS TEST!")
+
+print(new_content)
 
 with open(file_path, "w") as file:
     file.write(new_content)
