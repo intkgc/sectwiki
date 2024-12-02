2ed7245761b27d456981d9c380770b076af52591
Kirill <kirbo06@mail.ru>
2024-12-02 19:46:17 +0100

    da

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: ["Source Sans Pro", "Noto Emoji"],
+        body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
       colors: {
